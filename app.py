f591d9308461021aefa7640ed0dfb853ef020db9
Nullzero <p4rlx-news@pm.me>
2023-02-13 07:34:45 +1030

    working

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-#Flask app for interacting with climbing instructor
-
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
 from chefbot import ask, append_interaction_to_chat_log
-import os
 
-account_sid = os.getenv('TWSID')
-auth_token = os.getenv('TW_API')
-
-client = Client(account_sid,auth_token)
 app = Flask(__name__)
 # if for some reason your conversation with the chef gets weird, change the secret key 
-app.config['SECRET_KEY'] = 'Falling!!!!'
+app.config['SECRET_KEY'] = 'awesomefun'
 
 @app.route('/chefbot', methods=['POST'])
 def chef():
@@ -21,13 +13,9 @@ def chef():
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
                                                          chat_log)
-    
     msg = MessagingResponse()
-    print(msg)
-    msg = msg.message(answer)
-
+    msg.message(answer)
     return str(msg)
-    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=4007, debug=True)
--- chefbot.py
+++ chefbot.py
@@ -1,49 +1,34 @@
-#Rock climbing instructor
 from dotenv import load_dotenv
+from random import choice
+from flask import Flask, request
 import os
 import openai
 
 load_dotenv()
-
-#Set openAI key
-def set_openai_key(key):
-    """Sets OpenAI key."""
-    openai.api_key = key
-
-#OpenAI key info
-KEY_NAME = os.getenv("OPENAI_KEY")
-set_openai_key(KEY_NAME)
-
+openai.api_key = os.environ.get('OPENAI_KEY')
 completion = openai.Completion()
 
-start_sequence = "\nmountainbot:"
+start_sequence = "\nClimber:"
 restart_sequence = "\n\nPerson:"
-session_prompt = '''Johnny is a mountaineering and rock climbing instructor that lives in Squamish, Canada. He speaks directly, and prefers to be honest 
-                    with people he speaks to because of safety reasons. He has lived all over the world and knows people like Alex Honnold, John Long and knew the Huber brothers.
-                    The instructor also knows Reinhold Messler well and likes to refer to his talents regularly. He has climbed all over the world including Yosemite and the Himalaya. 
-                    Two highlights including The Nose on El Capitan, and Ama Dablam in the Himalaya.'''
+session_prompt = '''You are a fantastic rock climbing guide who lives in Squamish and has 
+                    climbed in the Himalaya over 25 years'''
 
 
-def ask(questionBot, chat_log = None):
-    questionBot = f'{chat_log}{restart_sequence}:{questionBot}{start_sequence}:'
-    completion = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt= questionBot,
-      temperature=0.4,
+def ask(question, chat_log=None):
+  prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
+  response = openai.Completion.create(
+      model="text-davinci-003",
+      temperature=0.1,  
       max_tokens=96,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0.3,
-      stop=["\n"],
-    )
-
-    response = completion.choices[0].text
-    return str(response)
-
-
+      stop=["\n"]
+  )
+  story = response['choices'][0]['text']
+  return str(story)
 
 def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None:
         chat_log = session_prompt
     return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
-
